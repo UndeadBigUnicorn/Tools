b169d51abd64463c4785b9f22e80cd308d4dcb03
Kravchenko Danylo <kravchel16@gmail.com>
2019-05-01 20:00:30 +0300

    removed mail credentials

--- config/config.js
+++ config/config.js
@@ -7,12 +7,7 @@ CONFIG.CONNECTION_URL = process.env.MONGODB_URI || "mongodb://localhost:27017/to
 CONFIG.PORT = process.env.PORT || 3000;
 
 //Mail config
-CONFIG.MAIL_SERVER = "smtp.postmarkapp.com";
-CONFIG.MAIL_PORT = 2525;
-CONFIG.MAIL_USER = "e5cc1b06-a10f-4042-94ac-d4737ad02d05";
-CONFIG.MAIL_PASSWORD = "e5cc1b06-a10f-4042-94ac-d4737ad02d05";
-//CONFIG.MAIL_SENDER = "support@webtools";
-CONFIG.MAIL_SENDER = "danil@xquestions.org";
+
 
 //Export our config
 module.exports = CONFIG;
