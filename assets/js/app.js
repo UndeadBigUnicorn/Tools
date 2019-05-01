503edf45142dbeb82d66e6ccd6741225f3ea344c
imaltsev <124finger@gmal.com>
2019-05-01 15:14:16 +0300

    frontend: add buttons, end tool design

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,26 @@ in_editor.session.on('change', function(delta) {
 	//transformation script
     out_editor.setValue(in_editor.getValue());
 });
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+$('#load_button').click(function(){
+    alert('lol');
+});
+
+$('#download_button').click(function(){
+    var text = out_editor.getValue();
+    var filename = "result.txt";
+    download(filename, text);
+});
