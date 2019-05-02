5f3e2cc22954d5fb64f730a04e23d5e05c23c61c
Kravchenko Danylo <kravchel16@gmail.com>
2019-05-02 23:56:48 +0300

    added input validation in login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,8 +2,40 @@ $(window).ready(()=>{
     
     $("#email").change(()=>{
         let email = $("#email").val();
-        if(email != ""){
-            
+        if(email == ""){
+            $("#email-err").text("Email is required!");
+            $("#email").addClass("is-danger");
         }
+        else if(!validateEmail(email)){
+            $("#email-err").text("Email is not correct!");
+            $("#email").addClass("is-danger");
+        }
+        else{
+            $("#email-err").text("");
+            $("#email").removeClass("is-danger");
+        }
+
     })
+
+    $("#password").change(()=>{
+        let password = $("#password").val();
+        if(password == ""){
+            $("#password-err").text("Password is required!");
+            $("#password").addClass("is-danger");
+        }
+        else if(password.length < 6){
+            $("#password-err").text("Password is too short!");
+            $("#password").addClass("is-danger");
+        }
+        else{
+            $("#password-err").text("");
+            $("#password").removeClass("is-danger");
+        }
+
+    })
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }
 })
