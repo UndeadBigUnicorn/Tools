725510f8bc361a44743ec674bcfc58d7c3a13318
Kravchenko Danylo <kravchel16@gmail.com>
2019-05-03 13:22:46 +0300

    added TODO in avatar generator

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -15,5 +15,35 @@ $(window).ready(()=>{
         }
     })
 
+    //TODO: change image drawing method to this binary one
+    getBase64FromImageUrl("http://avatar-generator-tool.herokuapp.com/s");
+
+    function getBase64FromImageUrl(url) {
+    var img = new Image();
+
+    img.setAttribute('crossOrigin', 'anonymous');
+
+    var data = "";
+
+    img.onload = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width =this.width;
+        canvas.height =this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this, 0, 0);
+
+        var dataURL = canvas.toDataURL("image/png");
+
+        data =  dataURL.replace(/^data:image\/(png|jpg);base64,/, "")
+        var img = document.createElement("img");
+        img.src = 'data:image/jpeg;base64,' + data;
+        document.body.appendChild(img);
+        //alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+    };
+
+    img.src = url;
+}
+
    
 })
