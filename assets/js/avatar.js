e2995662a7f483e02a24fe06bbd34a2894d851ac
Kravchenko Danylo <kravchel16@gmail.com>
2019-05-03 12:35:19 +0300

    fixed multiply download button in avatar generator

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -4,10 +4,15 @@ $(window).ready(()=>{
         let name = $("#name").val();
         $("#temp").hide();
         $("#image").attr("src",`http://avatar-generator-tool.herokuapp.com/${name}`);
-        $("#temp-container").append(`
-        <div class="control">
-        <a href="http://avatar-generator-tool.herokuapp.com/download/${name}" class="button is-info">Download</a>
-        </div>`);
+        if($("#download-btn").length == 0){
+            $("#temp-container").append(`
+            <div class="control">
+            <a id="download-btn" href="http://avatar-generator-tool.herokuapp.com/download/${name}" class="button is-info">Download</a>
+            </div>`);
+        }
+        else {
+            $("#download-btn").attr("href", `http://avatar-generator-tool.herokuapp.com/download/${name}`);
+        }
     })
 
    
