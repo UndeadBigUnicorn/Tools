b13131ebd3fd39c30b549fecacc121f293915631
Kravchenko Danylo <kravchel16@gmail.com>
2019-05-03 16:11:49 +0300

    rewrite image drawing and downloading to use binary data, added loader animation

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -1,48 +1,49 @@
 $(window).ready(()=>{
 
+    $("#loader").hide();
+
     $("#confirm-btn").click(()=>{
         let name = $("#name").val();
         $("#temp").hide();
-        $("#image").attr("src",`http://avatar-generator-tool.herokuapp.com/${name}`);
-        if($("#download-btn").length == 0){
-            $("#temp-container").append(`
-            <div class="control">
-            <a id="download-btn" href="http://avatar-generator-tool.herokuapp.com/download/${name}" class="button is-info">Download</a>
-            </div>`);
-        }
-        else {
-            $("#download-btn").attr("href", `http://avatar-generator-tool.herokuapp.com/download/${name}`);
-        }
+
+        $("#loader").show();
+        $("#image").attr("src", "");
+        getBase64FromImageUrl(`http://avatar-generator-tool.herokuapp.com/${name}`);
     })
 
-    //TODO: change image drawing method to this binary one
-    getBase64FromImageUrl("http://avatar-generator-tool.herokuapp.com/s");
-
     function getBase64FromImageUrl(url) {
-    var img = new Image();
+        var img = new Image();
 
-    img.setAttribute('crossOrigin', 'anonymous');
+        img.setAttribute('crossOrigin', 'anonymous');
 
-    var data = "";
+        img.onload = function () {
+            $("#loader").hide();
+            var canvas = document.createElement("canvas");
+            canvas.width =this.width;
+            canvas.height =this.height;
 
-    img.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width =this.width;
-        canvas.height =this.height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(this, 0, 0);
 
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
+            var dataURL = canvas.toDataURL("image/png");
 
-        var dataURL = canvas.toDataURL("image/png");
+            let data =  dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 
-        data =  dataURL.replace(/^data:image\/(png|jpg);base64,/, "")
-        var img = document.createElement("img");
-        img.src = 'data:image/jpeg;base64,' + data;
-        document.body.appendChild(img);
-        //alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-    };
+            $("#image").attr("src", 'data:image/jpeg;base64,' + data);
 
-    img.src = url;
+            data = 'data:image/jpeg;base64,' + data;
+            if($("#download-btn").length == 0){
+                $("#temp-container").append(`
+                <div class="control">
+                <button id="download-btn" onclick="download(); return false;" data-src="${data}" class="button is-info">Download</button>
+                </div>`);
+            }
+            else {
+                $("#download-btn").attr("data-src", data);
+            }
+        };
+
+        img.src = url;
 }
 
    
